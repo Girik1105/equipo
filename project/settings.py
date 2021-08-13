a359f800d787927e50740b780bb72d4e16b9501b
Girik1105 <girik1105@gmail.com>
2021-08-14 00:00:41 +0530

    fixed production bugs

--- project/settings.py
+++ project/settings.py
@@ -174,7 +174,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_venv')]
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

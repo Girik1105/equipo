0b5e0768362ed864b24aa2b9849ab7e1be127416
Girik1105 <girik1105@gmail.com>
2021-08-13 23:12:12 +0530

    added whitenoise

--- project/settings.py
+++ project/settings.py
@@ -76,6 +76,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [

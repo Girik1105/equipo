ec4f33a72fa2ac9edac3f4d8562df5ef412f8d17
HansYoloMF <duaindude@gmail.com>
2021-08-13 16:24:24 +0530

    3 web pages
    sign in ,sign up and features pages

---
+++ extras/frontend/School Hackathon/Sign up page/script.js
@@ -0,0 +1,16 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+x = true;
+if ((x = true)) {
+  container.classList.add("sign-up-mode");
+} else {
+}
+//
+///sign_up_btn.addEventListener("click", () => {
+// container.classList.add("sign-up-mode");
+//});
+
+//gn_in_btn.addEventListener("click", () => {
+//  container.classList.remove("sign-up-mode");
+//});

1b08a4c952f3dcb41b2f2aef0e646370314a325e
Girik1105 <girik1105@gmail.com>
2021-08-14 16:19:07 +0530

    media files fix

--- project/urls.py
+++ project/urls.py
@@ -36,12 +36,11 @@ urlpatterns = [
 
     path('', views.index, name="index"),
 
-    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
 ]
 
 handler404 = 'project.views.error_404'
 handler403 = 'project.views.error_403'
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

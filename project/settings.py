f30e0c9938c9726971120c4b124651d28127f8a3
Girik1105 <girik1105@gmail.com>
2021-08-16 12:53:31 +0530

    production ready

--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key() 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['equipo-project.herokuapp.com', '127.0.0.1']
 
